2d1b0b581b9c11a5f07782890036c7a35ddbd5c5
MM2Markov <makshockey13@gmail.com>
2019-02-22 22:43:45 +0300

    Added scripts, MM2

---
+++ JAM_GAME/Assets/CameraRot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRot : MonoBehaviour
+{
+  public GameObject go;
+  public float sensitivity = 1F;
+  private Camera goCamera;
+  private Vector3 MousePos;
+  private float MyAngle = 0F;
+  // Start is called before the first frame update
+    void Start()
+    {
+    goCamera = Camera;
+    go = goCamera.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    MousePos = Input.mousePosition;
+    }
+
+  void FixedUpdate()
+  {
+    if (Input.GetMouseButton(1))
+    {
+      MyAngle = 0;
+      MyAngle = sensitivity * ((MousePos.x - (Screen.width / 2)) / Screen.width);
+      goCamera.transform.RotateAround(go.transform.position, goCamera.transform.up, MyAngle);
+      MyAngle = sensitivity * ((MousePos.y - (Screen.height / 2)) / Screen.height);
+      goCamera.transform.RotateAround(go.transform.position, goCamera.transform.right, -MyAngle);
+    }
+  }
+}
