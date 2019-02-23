536f3e71cdc3212832a8fb5f1e29d428191304a7
MM2Markov <makshockey13@gmail.com>
2019-02-23 13:06:21 +0300

    Commit and pushing old

---
+++ JAM_GAME/Assets/Scripts/Screame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Screame : MonoBehaviour
+{
+  public AudioClip screamS;
+  public Material screamImage;
+  void OnCollisionEnter(Collision ScreamTrigger)
+  {
+    if (ScreamTrigger.gameObject.name == "Cube")
+    {
+      gameObject.GetComponent<Renderer>().material = screamImage;
+      AudioSource.PlayClipAtPoint(screamS, transform.position);
+      Debug.Log("NOOOOOOOP");
+    }
+  }
+}
