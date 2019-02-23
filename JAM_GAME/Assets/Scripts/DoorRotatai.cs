c039c913eb5ead60779c251e69fb3f9dbbffd99f
MM2Markov <makshockey13@gmail.com>
2019-02-23 19:02:09 +0300

    Added doors opening(not closing), MM2

---
+++ JAM_GAME/Assets/Scripts/DoorRotatai.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorRotatai : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Cube")
+          transform.rotation = new Quaternion(0, 1f, 0, 1);
+    }
+    /*void OnCollisionExit()
+    {
+        transform.rotation = new Quaternion(0, 0, 0, 1); 
+    } */
+}
---
+++ JAM_GAME/Assets/Scripts/DoorRotationPaul.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorRotationPaul : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Cube")
+          transform.rotation = new Quaternion(0, -1f, 0, 1);
+    }
+    /*void OnCollisionExit()
+    {
+        transform.rotation = new Quaternion(0, 0, 0, 1); 
+    }*/
+}
